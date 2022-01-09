d98cde470556e4a61dc5f3e577a4b45d7222162c
noam amron <noam0574@gmail.com>
2022-01-09 20:34:17 +0200

    add more info functionality, save and remove items from session storage

--- scripts/coins.js
+++ scripts/coins.js
@@ -1,39 +1,46 @@
-// const Coins = () => {
-// $(() => {
 const URL = "https://api.coingecko.com/api/v3/coins";
+const coinsCardContainerElement = $("#main-container");
+let selectedCoinsCounter = 0;
 
 const displayCoins = async () => {
-  //   console.log("Hello World");
-  //   $("main").append("New content will be in here!");
-  let cardsElements = "";
-  const cardsRow = document.createElement("div");
-  cardsRow.classList.add("row", "row-cols-1", "row-cols-sm-2", "row-cols-md-4");
-  await getCoinsList(`${URL}/list`)
+  let cardsElements;
+
+  coinsCardContainerElement.empty();
+  await getDataFromServer(`${URL}/list`)
     .then((data) => {
-      console.log(data.length);
-      for (let i = 0; i < data.length; i++) {
-        cardsElements += `
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">${data[i].symbol.toUpperCase()}
-                    <div class="form-check form-switch card-title__switch-container">
-                        <input class="form-check-input card-title__switch-input" type="checkbox" role="switch" id="card-title__switch${i}">
-                    </div>
-                    </h5>
-                    
-                    <p class="card-text">${data[i].name}</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>`;
-      }
-      $("#main-container").append(cardsElements);
+      cardsElements = createCardsElements(data);
+      coinsCardContainerElement.append(cardsElements);
     })
     .catch((e) => {
       console.log(e);
     });
 };
 
-const getCoinsList = (url) => {
+const createCardsElements = (coinsArray) => {
+  let cards = "";
+  for (let i = 0; i < 100; i++) {
+    cards += `<div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">${coinsArray[
+                      i
+                    ].symbol.toUpperCase()}</h5>
+                    <div class="form-check form-switch card-title__switch-container">
+                        <input class="form-check-input card-title__switch-input" type="checkbox" role="switch" id="card-title__switch${i}">
+                    </div>
+                    <p class="card-text">${coinsArray[i].name}</p>
+                    <button class="btn btn-primary card__more-info-button" id = "${
+                      coinsArray[i].id
+                    }">More info</button>
+                    <div class = "card__coin-info-container" id = "coin-info-${
+                      coinsArray[i].id
+                    }" ></div>
+                </div>
+            </div>`;
+  }
+  return cards;
+};
+
+const getDataFromServer = (url) => {
   const coinsPromise = new Promise((resolve, reject) => {
     $.ajax({
       url: url,
@@ -49,9 +56,52 @@ const getCoinsList = (url) => {
   });
   return coinsPromise;
 };
-// });
+
+coinsCardContainerElement.on(
+  "click",
+  ".card__more-info-button",
+  async (event) => {
+    const coinId = event.currentTarget.id;
+    const infoContainer = $(`#coin-info-${coinId}`);
+    infoContainer.empty();
+    let coinInfo = {};
+
+    if (sessionStorage.getItem(`${coinId}`)) {
+      coinInfo = JSON.parse(sessionStorage.getItem(`${coinId}`));
+    } else {
+      await getDataFromServer(`${URL}/${coinId}`).then((info) => {
+        coinInfo = {
+          img: info.image.small,
+          usd: info.market_data.current_price.usd,
+          nis: info.market_data.current_price.ils,
+          eur: info.market_data.current_price.eur,
+        };
+        saveInSessionStorage(coinInfo, coinId);
+      });
+    }
+    toggleCoinInfo(infoContainer, coinInfo);
+  }
+);
+
+const toggleCoinInfo = (containerElement, coinObject) => {
+  containerElement.append(`
+      <div class = "card__coin-info-image">
+          <img src = "${coinObject.img}"/>
+      </div>
+      <ul class = "card__coin-info-list">
+          <li class = "card__coin-info-list-item"> Price in dollars: ${coinObject.usd} &#36;</li>
+          <li class = "card__coin-info-list-item"> Price in new israeli shekel: ${coinObject.nis} &#8362;</li>
+          <li class = "card__coin-info-list-item"> Price in euro: ${coinObject.eur} &#8364;</li>
+      </ul>
+  `);
+  containerElement.slideToggle(2000);
+};
+
+const saveInSessionStorage = (objectToSave, coinObjId) => {
+  sessionStorage.setItem(`${coinObjId}`, JSON.stringify(objectToSave));
+  setTimeout(() => {
+    sessionStorage.removeItem(`${coinObjId}`);
+  }, 12000);
+};
 
 export { displayCoins };
-// };
-
-// main-container
